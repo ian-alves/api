136d0ab9a94d82680b8ad3997a04b1e2b3507c23
Ian Alves <ianh.alves@gmail.com>
2022-08-31 11:20:51 -0300

    classe usuário

---
+++ src/main/java/com/example/api/domain/User.java
@@ -0,0 +1,30 @@
+package com.example.api.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// @Data substitui o: @Getter, @Setter, @EqualsAndHashCode, @ToString
+// EqualsAndHashCode é mais performático fazer manualmente, pois seleciona só o id para comparação
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
